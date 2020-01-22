12a86828433a101e4672bb8fd46806e1014581ab
jsmithxyz <jordansmith3813@gmail.com>
2020-01-21 23:36:37 -0500

    changed color scheme, added photos, modal, and bio text

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ $(document).ready(function() {
   
 $('.carousel').carousel({
     'indicators': true,
+
 });
 
 $('.tooltipped').tooltip();
 
+$('.modal').modal();
+
+$('.scrollspy').scrollSpy();
 
 
 
