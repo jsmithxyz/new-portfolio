aa237ed634cb3fb61ddf6823d612f99a38625d2c
jsmithxyz <jordansmith3813@gmail.com>
2020-01-22 22:07:55 -0500

    added portfolio functionality, linked repos

--- script.js
+++ script.js
@@ -12,4 +12,14 @@ $('.scrollspy').scrollSpy({
     'scrollOffset': 0,
 });
 
+$(".projectItem").hover(function(){
+    $(this).css("border-color", '#1ac6ff');
+    $(this).css("opacity", '0.5');
+
+}, function(){
+    $(this).css("border-color", 'black');
+    $(this).css("opacity", '1.0');
+
+});
+
 })
